d2990462823b1e1c8bb772247d58be55127c20de
morcsanyitamas <mtomi8@gmail.com>
2021-04-12 09:49:45 +0200

    add initial code

---
+++ adder.py
@@ -0,0 +1,17 @@
+def get_sum(bit_a, bit_b, carry):
+    pass
+
+
+def get_carry(bit_a, bit_b, carry):
+    pass
+
+
+def add_bits(input_carry, bit_a, bit_b):
+    pass
+
+
+def add(a, b):
+    pass
+
+
+print(add(5, 3))
