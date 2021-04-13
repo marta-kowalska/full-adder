3b0fb5dac4ca5e1f4b6ad585275a736a97d13cdb
morcsanyitamas <mtomi8@gmail.com>
2021-04-13 17:02:49 +0200

    adder done

--- adder.py
+++ adder.py
@@ -1,17 +1,60 @@
 def get_sum(bit_a, bit_b, carry):
-    pass
+    return (bit_a ^ bit_b ^ carry)
 
 
 def get_carry(bit_a, bit_b, carry):
-    pass
+    return (bit_a and bit_b) or \
+           (not bit_a and bit_b and carry) or \
+           (bit_a and not bit_b and carry)
 
 
-def add_bits(input_carry, bit_a, bit_b):
-    pass
+def get_bit(number, bit_position):
+    number = number >> bit_position
+    number = number & 1
+    return number
+
+
+def set_bit(number, bit_position):
+    mask = 1 << bit_position
+    number = number | mask
+    return number 
+
+
+'''
+XOR
+a b o
+0 0 0
+0 1 1
+1 0 1
+1 1 0
+'''
+
+
+
+
+def get_number(number_bits):
+    number_str = ''
+    for bit in number_bits:
+        number_str += str(bit)
+    return int(number_str, 2)
 
 
 def add(a, b):
-    pass
+    result = []
+    carry = 0
+    bit_a = get_bit(a, 0)
+    bit_b = get_bit(b, 0)
+    while(carry or bit_a or bit_b):
+        sum_bits = get_sum(bit_a, bit_b, carry)
+        carry = get_carry(bit_a, bit_b, carry)
+        result.insert(0, sum_bits)
+        a = a >> 1
+        b = b >> 1
+        bit_a = get_bit(a, 0)
+        bit_b = get_bit(b, 0)
+    return get_number(result)
 
 
-print(add(5, 3))
+
+
+print(add(7, 11))
